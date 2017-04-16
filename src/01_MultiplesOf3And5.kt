c85000d3f865e837d819748c46428d5c0c0d3671
Sebastian Gebert <achondrias@gmail.com>
2017-04-17 01:06:26 +0200

    Problem 1 solved. Holy fuck I love Kotlin.

--- src/01_MultiplesOf3And5.kt
+++ src/01_MultiplesOf3And5.kt
@@ -3,3 +3,9 @@
  * The sum of these multiples is 23.
  * Find the sum of all the multiples of 3 or 5 below 1000.
  */
+
+fun multiples() = (1..1000).toList().filter { it % 3 == 0 || it % 5 == 0 }
+
+fun main(args: Array<String>) {
+    multiples().forEach(System.out::println)
+}
