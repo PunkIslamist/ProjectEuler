34c5a5200c18414d5be11331c1598a7d1fbd98b1
Sebastian Gebert <achondrias@gmail.com>
2017-04-17 13:17:04 +0200

    Fix: forgot to sum up the numbers.

--- src/01_MultiplesOf3And5.kt
+++ src/01_MultiplesOf3And5.kt
@@ -4,8 +4,8 @@
  * Find the sum of all the multiples of 3 or 5 below 1000.
  */
 
-fun multiples() = (1..1000).toList().filter { it % 3 == 0 || it % 5 == 0 }
+fun sumdidum() = (1..1000).toList().filter { it % 3 == 0 || it % 5 == 0 }.sum()
 
 fun main(args: Array<String>) {
-    multiples().forEach(System.out::println)
+    System.out.println(sumdidum())
 }
